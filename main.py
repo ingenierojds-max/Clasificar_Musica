0cf986087a526501f2215d2025fca9bb3cd12b1a
ingenierojds-max <ingenierojds@gmail.com>
2025-08-03 20:58:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, File, UploadFile
-from mutagen import File as MutagenFile
-import uvicorn
+from mutagen.mp3 import MP3
+import os
 
 app = FastAPI()
 
 @app.post("/metadata")
 async def get_metadata(file: UploadFile = File(...)):
+    temp_filename = "temp.mp3"
     contents = await file.read()
-    with open("temp.mp3", "wb") as f:
+    with open(temp_filename, "wb") as f:
         f.write(contents)
 
-    audio = MutagenFile("temp.mp3", easy=True)
-    metadata = dict(audio.tags) if audio and audio.tags else {}
-    return {"filename": file.filename, "metadata": metadata}
+    audio = MP3(temp_filename)
+    os.remove(temp_filename)
+
+    return {
+        "filename": file.filename,
+        "duration_seconds": round(audio.info.length, 2),
+        "bitrate": audio.info.bitrate
+    }
